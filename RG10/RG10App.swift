70cb87d0818751dbd85c336bf3a943645fcd8283
Moneeb S. Sayed <moneebssayed@gmail.com>
2025-07-04 11:46:08 -0400

    Initial Commit

---
+++ RG10/RG10App.swift
@@ -0,0 +1,17 @@
+//
+//  RG10App.swift
+//  RG10
+//
+//  Created by Moneeb Sayed on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct RG10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RG10Tests/RG10Tests.swift
@@ -0,0 +1,17 @@
+//
+//  RG10Tests.swift
+//  RG10Tests
+//
+//  Created by Moneeb Sayed on 7/4/25.
+//
+
+import Testing
+@testable import RG10
+
+struct RG10Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
